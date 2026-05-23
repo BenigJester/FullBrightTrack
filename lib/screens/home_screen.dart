5faabfb7bc69df5ed30ad4db9556114647171232
BenigJester <benigjester@gmail.com>
2026-05-23 10:14:40 +0800

    Remove a comment import

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
 import '../services/auth_service.dart';
 import 'home_tab.dart';
 import 'steps_tab.dart';
